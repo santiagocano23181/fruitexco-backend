99fccf187ca2b478267dedc56736c0083789e6b7
Santiago Cano <sc@smartalentit.com>
2022-09-18 19:16:34 -0500

    Cambios backend

--- src/models/Sale/Domicile.py
+++ src/models/Sale/Domicile.py
@@ -7,7 +7,7 @@ class Domicile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Numeric, nullable=False)
     start_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    finish_date = db.Column(db.DateTime)
+    finish_date = db.Column(db.DateTime, nullable=False)
     
     def __repr__(self) -> str:
         return "<Domicile %r>" % self.price
