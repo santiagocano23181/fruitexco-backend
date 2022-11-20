a9e2ef342d97ee7b706cc702f0b9f1ecbb351082
Santiago Cano Ospina <82837819+SantiagoC4n0@users.noreply.github.com>
2022-11-20 14:53:58 -0500

    postgresql

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='localhost')
+    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL_POSTGRESQL', default='localhost')
     TESTING = True
     
 config = {
