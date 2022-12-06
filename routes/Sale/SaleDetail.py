9dc8dcbce3ecf4005c4e741b2fd25e494fa1af32
Santiago Cano Ospina <82837819+SantiagoC4n0@users.noreply.github.com>
2022-12-05 22:13:45 -0500

    final parts

--- routes/Sale/SaleDetail.py
+++ routes/Sale/SaleDetail.py
@@ -12,12 +12,12 @@ from marshmallow_sqlalchemy.fields import Nested
 from utils.db import db
 from utils.ma import ma
 
-sale_detail = Blueprint('sale_detail', __name__)
+sale_detail = Blueprint("sale_detail", __name__)
 
 
 class MesureSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name')
+        fields = ("id", "name")
 
 
 mesure_schema = MesureSchema()
@@ -26,7 +26,7 @@ many_status_schema = MesureSchema(many=True)
 
 class TasteSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name')
+        fields = ("id", "name")
 
 
 taste_schema = TasteSchema()
@@ -35,7 +35,7 @@ many_taste_schema = TasteSchema(many=True)
 
 class StatusProductSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name')
+        fields = ("id", "name")
 
 
 status_schema = StatusProductSchema()
@@ -44,7 +44,7 @@ many_status_schema = StatusProductSchema(many=True)
 
 class SectionSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'description')
+        fields = ("id", "name", "description")
 
 
 section_schema = SectionSchema()
@@ -56,10 +56,35 @@ class ProductSchema(ma.SQLAlchemyAutoSchema):
         model = Products
         include_fk = True
         load_instance = True
+
     mesure = Nested(MesureSchema)
     taste = Nested(TasteSchema)
     section = Nested(SectionSchema)
     status = Nested(StatusProductSchema)
+
+
+class DiscountSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Discount
+        include_fk = True
+        load_instance = True
+
+
+discount_schema = DiscountSchema()
+many_discount_schema = DiscountSchema(many=True)
+
+
+class DiscountDetailSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = DiscountDetail
+        include_fk = True
+        load_instance = True
+
+    discount = Nested(DiscountSchema)
+
+
+discount_detail_schema = DiscountDetailSchema()
+many_discount_detail_schema = DiscountDetailSchema(many=True)
 
 
 class SaleDetailSchema(ma.SQLAlchemyAutoSchema):
@@ -67,37 +92,49 @@ class SaleDetailSchema(ma.SQLAlchemyAutoSchema):
         model = SaleDetail
         include_fk = True
         load_instance = True
+
     products = Nested(ProductSchema)
+    discount_detail = Nested(DiscountDetailSchema, many=True)
 
 
 sale_detail_schema = SaleDetailSchema()
 many_sale_detail_schema = SaleDetailSchema(many=True)
 
 
-@sale_detail.route('/', methods=['POST'])
+@sale_detail.route("/", methods=["POST"])
 def create_sale_detail():
     try:
-        client_id = session.get('Authorization')
-        sale_status = SaleStatus.query.filter_by(name='EN CARRITO').first()
-        sale = Sales.query.filter(and_(Sales.status_id==sale_status.id, Sales.client_id==client_id)).order_by(
-            Sales.created_on).first()
-        print(sale)
-        sale_details = SaleDetail.query.filter(and_(
-            SaleDetail.sale_id==sale.id, SaleDetail.products_id==request.json['product_id'])).first()
-        print(sale_details)
+        client_id = session.get("Authorization")
+        sale_status = SaleStatus.query.filter_by(name="EN CARRITO").first()
+        sale = (
+            Sales.query.filter(
+                and_(Sales.status_id == sale_status.id, Sales.client_id == client_id)
+            )
+            .order_by(Sales.created_on)
+            .first()
+        )
+        sale_details = SaleDetail.query.filter(
+            and_(
+                SaleDetail.sale_id == sale.id,
+                SaleDetail.products_id == request.json["product_id"],
+            )
+        ).first()
         if sale_details == None:
-            sale_details = SaleDetail(sale.id, 
-                                      request.json['product_id'], 
-                                      request.json['cantity'])
+            sale_details = SaleDetail(
+                sale.id, request.json["product_id"], request.json["cantity"]
+            )
             db.session.add(sale_details)
-            print(sale_details)
         else:
-            sale_details.cantity = sale_details.cantity + request.json['cantity']
+            sale_details.cantity = sale_details.cantity + request.json["cantity"]
         discount = Discount.query.filter_by(finish_date=None).first()
         discount_detail = DiscountDetail.query.filter(
-            and_(DiscountDetail.discount_id==discount.id, DiscountDetail.sale_details_id==sale_details.id)).first()
+            and_(
+                DiscountDetail.discount_id == discount.id,
+                DiscountDetail.sale_details_id == sale_details.id,
+            )
+        ).first()
         if discount_detail == None:
-            if(discount.needed <= request.json['cantity']):
+            if discount.needed <= request.json["cantity"]:
                 discount_detail = DiscountDetail(discount.id, sale_details.id)
                 db.session.add(discount_detail)
         db.session.commit()
@@ -105,63 +142,86 @@ def create_sale_detail():
     except Exception as ex:
         return jsonify(messages=str(ex), context=3), 500
 
-@sale_detail.route('/')
+
+@sale_detail.route("/")
 def get_sale_detail():
+        id = session.get("Authorization")
+        sale_status = SaleStatus.query.filter_by(name="EN CARRITO").first()
+        sale = (
+            Sales.query.filter(
+                and_(Sales.status_id == sale_status.id, Sales.client_id == id)
+            )
+            .order_by(Sales.created_on)
+            .first()
+        )
+        sale_details = SaleDetail.query.filter(SaleDetail.sale_id == sale.id).all()
+
+        return many_sale_detail_schema.jsonify(sale_details)
+
+
+@sale_detail.route("/<id>")
+def get_sale_detail_by_sale_id(id):
     try:
-        id = session.get('Authorization')
-        sale_status = SaleStatus.query.filter_by(name='EN CARRITO').first()
-        sale = Sales.query.filter(and_(Sales.status_id==sale_status.id, Sales.client_id==id)).order_by(
-            Sales.created_on).first()
-        sale_details = SaleDetail.query.filter(SaleDetail.sale_id==sale.id).all()
-        
+        sale = Sales.query.get(id)
+        sale_details = SaleDetail.query.filter(SaleDetail.sale_id == sale.id).all()
+
         return many_sale_detail_schema.jsonify(sale_details)
     except Exception as ex:
         return jsonify(messages=str(ex), context=3), 500
 
 
-@sale_detail.route('/cantitiy/<id>', methods=['PATCH'])
+@sale_detail.route("/cantity/<id>", methods=["PATCH"])
 def update_cantity_sale_detail(id):
-    try:
-        user_id = session.get('Authorization')
-        sale_status = SaleStatus.query.filter_by(name='EN CARRITO').first()
-        sale = Sales.query.filter(and_(Sales.status_id==sale_status.id, Sales.client_id==user_id)).order_by(Sales.created_on).first()
-        sale_details = SaleDetail.query.filte(and_(SaleDetail.sale_id==sale.id, SaleDetail.products_id==id)).first()
-        sale_details.cantity = request.json['cantity']
+    sale_details = SaleDetail.query.get(id)
+    sale_details.cantity = request.json["cantity"]
 
-        discount = Discount.query.filter_by(finish_date=None).first()
-        db.session.add(sale_details)
+    discount = Discount.query.filter_by(finish_date=None).first()
+    db.session.add(sale_details)
 
-        discount_detail = DiscountDetail.query.filter(
-            and_(discount.id, sale_details.id)).first()
-        if(discount.needed <= request.json['cantity']):
-            if(discount_detail == None):
-                discount_detail = DiscountDetail(discount.id, sale_details.id)
-                db.session.add(discount_detail)
-        else:
-            if(discount_detail != None):
-                db.session.delete(discount_detail)
-        db.session.commit()
-        return sale_detail_schema.jsonify(sale_detail)
-    except Exception as ex:
-        return jsonify(messages=str(ex), context=3), 500
+    discount_detail = DiscountDetail.query.filter(
+        and_(
+            DiscountDetail.discount_id == discount.id,
+            DiscountDetail.sale_details_id == sale_details.id,
+        )
+    ).first()
+    if discount.needed <= request.json["cantity"]:
+        if discount_detail == None:
+            discount_detail = DiscountDetail(discount.id, sale_details.id)
+            db.session.add(discount_detail)
+    else:
+        if discount_detail != None:
+            db.session.delete(discount_detail)
+    db.session.commit()
+    return sale_detail_schema.jsonify(sale_detail)
 
 
-@sale_detail.route('/<id>', methods=['DELETE'])
+@sale_detail.route("/<id>", methods=["DELETE"])
 def delete_sale_detail(id):
     try:
-        user_id = session.get('Authorization')
-        sale_status = SaleStatus.query.filter_by(name='EN CARRITO').first()
-        sale = Sales.query.filter(and_(Sales.status_id==sale_status.id, Sales.client_id==user_id)).order_by(Sales.created_on).first()
-        sale_details = SaleDetail.query.filter(and_(SaleDetail.sale_id==sale.id, SaleDetail.products_id==id)).first()
+        user_id = session.get("Authorization")
+        sale_status = SaleStatus.query.filter_by(name="EN CARRITO").first()
+        sale = (
+            Sales.query.filter(
+                and_(Sales.status_id == sale_status.id, Sales.client_id == user_id)
+            )
+            .order_by(Sales.created_on)
+            .first()
+        )
+        sale_details = SaleDetail.query.get(id)
 
         discount = Discount.query.filter_by(finish_date=None).first()
 
         discount_detail = DiscountDetail.query.filter(
-            and_(discount.id, sale_details.id)).first()
-        if(discount_detail != None):
+            and_(
+                DiscountDetail.discount_id == discount.id,
+                DiscountDetail.sale_details_id == sale_details.id,
+            )
+        ).first()
+        if discount_detail != None:
             db.session.delete(discount_detail)
         db.session.delete(sale_details)
         db.session.commit()
         return sale_detail_schema.jsonify(sale_detail)
     except Exception as ex:
         return jsonify(messages=str(ex), context=3), 500
+
